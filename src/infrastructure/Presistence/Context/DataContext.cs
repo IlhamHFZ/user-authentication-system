c7d20c5adc5c64a1a8c11182b3770e3a63f1687b
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-08-30 13:38:50 +0700

    create DataContext class and create model entity

---
+++ src/infrastructure/Presistence/Context/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entites;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Presistence.Context;
+/*
+	TODO:
+	- ganti Class1 jadi nama class untuk database
+	- install nuget yang diperluin untuk code fisrt EF
+	- pake fluent api
+*/
+public class DataContext : IdentityDbContext<User, Role, Guid>
+{
+	public DataContext(DbContextOptions<DataContext> options): base(options)
+	{
+		
+	}
+	
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+		
+		modelBuilder.Entity<User>(user =>
+		{
+			user.Property(u => u.DisplayName).IsRequired();
+		});
+	}
+}
