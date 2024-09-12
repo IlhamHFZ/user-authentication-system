b847eb3ba70c55241d6346e790546b25246c4bd3
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-09-12 11:50:20 +0700

    add implement IUnitofWork to UnitofWork

---
+++ src/infrastructure/Presistence/Repository/UnitofWork.cs
@@ -0,0 +1,25 @@
+using Application.Repository.Interface;
+using Presistence.Context;
+using Presistence.Repository.Interface;
+
+namespace Presistence.Repository;
+
+public class UnitofWork : IUnitofWork
+{
+	private readonly DataContext _context;
+	
+	public UnitofWork(DataContext context)
+	{
+		_context = context;
+	}
+
+	public async Task SaveChangeAsync()
+	{
+		await _context.SaveChangesAsync();
+	}
+
+	public IRepository<T> Repository<T>() where T : class
+	{
+		return  new Repository<T>(_context);
+	}
+}
