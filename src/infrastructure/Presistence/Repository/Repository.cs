5d591bd5e17bfc0718facbb9b6ca970368b5c22c
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-09-12 11:49:32 +0700

    add implement IRepository to Repository

---
+++ src/infrastructure/Presistence/Repository/Repository.cs
@@ -0,0 +1,42 @@
+using Application.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using Presistence.Context;
+
+namespace Presistence.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+	private readonly DataContext _context;
+	private readonly DbSet<T> _set;
+	
+	public Repository(DataContext context)
+	{
+		_context = context;
+		_set = _context.Set<T>();
+	}
+	
+	public async Task CreateAsync(T entity)
+	{
+		await _set.AddAsync(entity);
+	}
+
+	public void DeleteAsync(T entity)
+	{
+		_set.Remove(entity);
+	}
+
+	public async Task<IEnumerable<T>> GetAllAsync()
+	{
+		return await _set.ToListAsync();
+	}
+
+	public async Task<T> GetAsync(Guid id)
+	{
+		return await _set.FindAsync(id);
+	}
+
+	public void UpdateAsync(T entity)
+	{
+		_set.Update(entity);
+	}
+}
