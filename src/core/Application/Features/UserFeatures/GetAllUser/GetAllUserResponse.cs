6f5dec2a21b9c6bed6fb76e0a7ba9350c967b90c
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-09-13 14:32:00 +0700

    add GetAllUserResponse

---
+++ src/core/Application/Features/UserFeatures/GetAllUser/GetAllUserResponse.cs
@@ -0,0 +1,13 @@
+namespace Application.Features.UserFeatures.GetAllUser;
+
+public record GetAllUserResponse
+{
+	public Guid Id { get; set; }
+	public string UserName { get; set; }
+	public string DisplayName { get; set; }
+	public string Email { get; set; }
+	public bool EmailConfirmed { get; set; }
+	public string PhoneNumber {get; set; }
+	public bool PhoneConfirmed { get; set; }
+	public bool TwoFactorEnable { get; set; }
+}
