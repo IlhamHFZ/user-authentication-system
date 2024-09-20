09b852bb559665f55e9194d976ec19910b53fffa
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-09-20 16:27:52 +0700

    edit Dependency GetAllUserHandler from IRepository to IUnitofWork

--- src/core/Application/Features/UserFeatures/GetAllUser/GetAllUserHandler.cs
+++ src/core/Application/Features/UserFeatures/GetAllUser/GetAllUserHandler.cs
@@ -1,23 +1,24 @@
 using Application.Repository.Interface;
 using AutoMapper;
 using Domain.Entites;
+using Presistence.Repository.Interface;
 
 namespace Application.Features.UserFeatures.GetAllUser;
 
 public class GetAllUserHandler
 {
-	private readonly IRepository<User> _repository;
+	private readonly IUnitofWork _unitofWork;
 	private readonly IMapper _mapper;
 	
-	public GetAllUserHandler(IRepository<User> repository, IMapper mapper)
+	public GetAllUserHandler(IUnitofWork unitofWork, IMapper mapper)
 	{
-		_repository = repository;
+		_unitofWork = unitofWork;
 		_mapper = mapper;
 	}
 	
 	public async Task<IEnumerable<GetAllUserResponse>> Handle()
 	{
-		var users = await _repository.GetAllAsync();
+		var users = await _unitofWork.Repository<User>().GetAllAsync();
 		return _mapper.Map<IEnumerable<GetAllUserResponse>>(users);
 	}
 }
