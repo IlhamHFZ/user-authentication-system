76ba4f3d70109b715b7a0c96220d5c9e876629ac
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-09-20 16:30:06 +0700

    edit dependency GetByIdUserHandler from IRepository to IUnitofWork

--- src/core/Application/Features/UserFeatures/GetByIdUser/GetByIdUserHandler.cs
+++ src/core/Application/Features/UserFeatures/GetByIdUser/GetByIdUserHandler.cs
@@ -1,23 +1,24 @@
 using Application.Repository.Interface;
 using AutoMapper;
 using Domain.Entites;
+using Presistence.Repository.Interface;
 
 namespace Application.Features.UserFeatures.GetByIdUser;
 
 public class GetByIdUserHandler
 {
-	public readonly IRepository<User> _repository;
+	private readonly IUnitofWork _unitofWork;
 	public readonly IMapper _mapper;
 
-	public GetByIdUserHandler(IRepository<User> repository, IMapper mapper)
+	public GetByIdUserHandler(IUnitofWork unitofWork, IMapper mapper)
 	{
-		_repository = repository;
+		_unitofWork = unitofWork;
 		_mapper = mapper;
 	}
 	
 	public async Task<GetByIdUserResponse> Handle(GetByIdUserRequest request)
 	{
-		var user = await _repository.GetAsync(request.Id);
+		var user = await _unitofWork.Repository<User>().GetAsync(request.Id);
 		return _mapper.Map<GetByIdUserResponse>(user);
 	}
 }
