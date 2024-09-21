f39141b48419e12a0597b803482cd25a124f0bbc
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-09-21 20:49:14 +0700

    add UpdateUserResponse

---
+++ src/core/Application/Features/UserFeatures/UpdateUser/UpdateUserResponse.cs
@@ -0,0 +1,12 @@
+using Domain.Entites;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace Application.Features.UserFeatures.UpdateUser;
+
+public record class UpdateUserResponse
+{
+	public string UserName {get; set;}
+	public string DisplayName {get; set;}
+	public Role Role {get; set;}
+	
+}
