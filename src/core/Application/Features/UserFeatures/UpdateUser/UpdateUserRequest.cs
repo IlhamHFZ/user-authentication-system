2b9598e6432d9fcc7ad4f17029f77ec3a3f765ab
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-09-21 10:52:34 +0700

    edit add property Id in UpdateUserResponse

--- src/core/Application/Features/UserFeatures/UpdateUser/UpdateUserRequest.cs
+++ src/core/Application/Features/UserFeatures/UpdateUser/UpdateUserRequest.cs
@@ -2,6 +2,7 @@ namespace Application.Features.UserFeatures.UpdateUser;
 
 public record class UpdateUserRequest
 {
+	public Guid Id {get; set;}
 	public string UserName {get; set;}
 	public string DisplayName {get; set;}
 }
