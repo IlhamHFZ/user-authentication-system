c6c00732e604737b3ba352ef8e6cfb2371df1066
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-09-21 14:04:01 +0700

    edit UpdateUserValidator remove NotEmpty for UserName and DisplayName

--- src/core/Application/Features/UserFeatures/UpdateUser/UpdateUserValidator.cs
+++ src/core/Application/Features/UserFeatures/UpdateUser/UpdateUserValidator.cs
@@ -8,12 +8,10 @@ public class UpdateUserValidator : AbstractValidator<UpdateUserRequest>
 	public UpdateUserValidator()
 	{
 		RuleFor(user => user.UserName)
-			.NotEmpty()
 			.MinimumLength(3)
 			.MaximumLength(20)
 			.Matches(@"^[a-zA-Z]+$");
 		RuleFor(user => user.DisplayName)
-			.NotEmpty()
 			.MinimumLength(3)
 			.MaximumLength(10);
 	}
