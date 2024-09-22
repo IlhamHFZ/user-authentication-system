1fdf733fb42d6d2db3ba8d8859958c5b78860b7a
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-09-22 12:43:19 +0700

    edit mapping IdentityUser.PasswordHash to CreateUserResponse.Password

--- src/core/Application/Features/UserFeatures/CreateUser/CreateUserMapper.cs
+++ src/core/Application/Features/UserFeatures/CreateUser/CreateUserMapper.cs
@@ -7,7 +7,8 @@ public class CreateUserMapper : Profile
 {
 	public CreateUserMapper()
 	{
-		CreateMap<CreateUserRequest, User>();
+		CreateMap<CreateUserRequest, User>()
+			.ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.Password));
 		CreateMap<User, CreateUserResponse>();
 	}
 }
