43a9110bd88eb9e0ff4e42966e870c5a1091c787
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-09-26 21:06:27 +0700

    create IUserFacade

---
+++ src/core/Application/Features/UserFeatures/IUserFacade.cs
@@ -0,0 +1,18 @@
+using Application.Features.UserFeatures.CreateUser;
+using Application.Features.UserFeatures.DeleteUser;
+using Application.Features.UserFeatures.GetAllUser;
+using Application.Features.UserFeatures.GetByIdUser;
+using Application.Features.UserFeatures.UpdateUser;
+using Application.Features.UserFeatures.UpdateUserProfile;
+
+namespace Application.Features.UserFeatures;
+
+public interface IUserFacade
+{
+	Task<CreateUserResponse> CreateUserAsync(CreateUserRequest request);
+	Task<DeleteUserResponse> DeleteUserAsync(DeleteUserRequest request);
+	Task<IEnumerable<GetAllUserResponse>> GetAllUserAsync(GetAllUserRequest request);
+	Task<GetByIdUserResponse> GetByIdUserAsync(GetByIdUserRequest request);
+	Task<UpdateUserResponse> UpdateUserAsync(UpdateUserRequest request);
+	Task<UpdateUserProfileResponse> UpdateUserProfileAsync(UpdateUserProfileRequest request); 
+}
