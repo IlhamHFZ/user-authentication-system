13c204c2fc1d388c3d083d859a7b645635d21c2e
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-09-26 10:14:15 +0700

    edit add IFeatureHandler to GetByIdUserHandler

--- src/core/Application/Features/UserFeatures/GetByIdUser/GetByIdUserHandler.cs
+++ src/core/Application/Features/UserFeatures/GetByIdUser/GetByIdUserHandler.cs
@@ -1,10 +1,11 @@
+using Application.Features.Interface;
 using AutoMapper;
 using Domain.Entites;
 using Presistence.Repository.Interface;
 
 namespace Application.Features.UserFeatures.GetByIdUser;
 
-public class GetByIdUserHandler
+public class GetByIdUserHandler : IFeatureHandler<GetByIdUserResponse, GetByIdUserRequest>
 {
 	private readonly IUnitofWork _unitofWork;
 	public readonly IMapper _mapper;
@@ -15,7 +16,7 @@ public class GetByIdUserHandler
 		_mapper = mapper;
 	}
 	
-	public async Task<GetByIdUserResponse> Handle(GetByIdUserRequest request)
+	public async Task<GetByIdUserResponse> HandleAsync(GetByIdUserRequest request)
 	{
 		var user = await _unitofWork.Repository<User>().GetAsync(request.Id);
 		return _mapper.Map<GetByIdUserResponse>(user);
