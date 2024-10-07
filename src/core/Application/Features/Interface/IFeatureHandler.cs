ad5a7933de065ad9cefaf581463137177f7221ae
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-10-07 10:42:10 +0700

    delete IFeatureHandler.cs

--- src/core/Application/Features/Interface/IFeatureHandler.cs
+++
@@ -1,8 +0,0 @@
-namespace Application.Features.Interface;
-
-public interface IFeatureHandler<TResponse, TRequest> 
-	where TResponse : class
-	where TRequest : class
-{
-	public Task<TResponse> HandleAsync(TRequest request);
-}
