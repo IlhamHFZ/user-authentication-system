52b04ada0eeff47e550ab9c2d1ca54ae3091c119
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-10-14 13:00:06 +0700

    edit GetByIdUser

--- src/core/Application/Features/UserFeatures/GetByIdUser/GetByIdUserHandler.cs
+++ src/core/Application/Features/UserFeatures/GetByIdUser/GetByIdUserHandler.cs
@@ -1,24 +1,41 @@
-using Application.Features.Interface;
+using Application.Features.UserFeatures.Interface;
 using AutoMapper;
 using Domain.Entites;
+using FluentValidation;
 using Presistence.Repository.Interface;
 
 namespace Application.Features.UserFeatures.GetByIdUser;
 
-public class GetByIdUserHandler : IFeatureHandler<GetByIdUserResponse, GetByIdUserRequest>
+public class GetByIdUserHandler : IGetByIdUserHandler
 {
 	private readonly IUnitofWork _unitofWork;
-	public readonly IMapper _mapper;
+	private readonly IMapper _mapper;
+	private readonly IValidator<GetByIdUserRequest> _validator;
 
-	public GetByIdUserHandler(IUnitofWork unitofWork, IMapper mapper)
+	public GetByIdUserHandler(
+		IUnitofWork unitofWork, 
+		IMapper mapper, 
+		IValidator<GetByIdUserRequest> validator)
 	{
 		_unitofWork = unitofWork;
 		_mapper = mapper;
+		_validator = validator;
 	}
-	
-	public async Task<GetByIdUserResponse> HandleAsync(GetByIdUserRequest request)
+
+	public async Task<GetByIdUserResponse?> HandleAsync(GetByIdUserRequest request)
 	{
+		var result = _validator.Validate(request);
+		if(!result.IsValid)
+		{
+			_validator.ValidateAndThrow(request);
+		}
+		
 		var user = await _unitofWork.Repository<User>().GetAsync(request.Id);
+		if(user is null)
+		{
+			return null;
+		}
+		
 		return _mapper.Map<GetByIdUserResponse>(user);
 	}
 }
