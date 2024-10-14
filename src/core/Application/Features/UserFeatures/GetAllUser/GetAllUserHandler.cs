7dabbc980701edd895b424bccafbc695dd44f105
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-10-14 12:52:30 +0700

    edit GetAllUserHandler

--- src/core/Application/Features/UserFeatures/GetAllUser/GetAllUserHandler.cs
+++ src/core/Application/Features/UserFeatures/GetAllUser/GetAllUserHandler.cs
@@ -1,11 +1,11 @@
-using Application.Features.Interface;
+using Application.Features.UserFeatures.Interface;
 using AutoMapper;
 using Domain.Entites;
 using Presistence.Repository.Interface;
 
 namespace Application.Features.UserFeatures.GetAllUser;
 
-public class GetAllUserHandler : IFeatureHandler<IEnumerable<GetAllUserResponse>, GetAllUserRequest>
+public class GetAllUserHandler : IGetAllUserHandler
 {
 	private readonly IUnitofWork _unitofWork;
 	private readonly IMapper _mapper;
@@ -16,9 +16,14 @@ public class GetAllUserHandler : IFeatureHandler<IEnumerable<GetAllUserResponse>
 		_mapper = mapper;
 	}
 	
-	public async Task<IEnumerable<GetAllUserResponse>> HandleAsync(GetAllUserRequest request)
+	public async Task<IEnumerable<GetAllUserResponse>?> HandleAsync()
 	{
 		var users = await _unitofWork.Repository<User>().GetAllAsync();
+		if(users is null)
+		{
+			return Enumerable.Empty<GetAllUserResponse>();
+		}
+		
 		return _mapper.Map<IEnumerable<GetAllUserResponse>>(users);
 	}
 }
