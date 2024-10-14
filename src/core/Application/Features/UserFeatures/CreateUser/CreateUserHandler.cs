271512d6805670c5ad37aef2097554f07a5cf619
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-10-14 12:40:50 +0700

    edit give dependency IValidator

--- src/core/Application/Features/UserFeatures/CreateUser/CreateUserHandler.cs
+++ src/core/Application/Features/UserFeatures/CreateUser/CreateUserHandler.cs
@@ -1,6 +1,7 @@
 using Application.Features.UserFeatures.Interface;
 using AutoMapper;
 using Domain.Entites;
+using FluentValidation;
 using Presistence.Repository.Interface;
 
 namespace Application.Features.UserFeatures.CreateUser;
@@ -9,28 +10,33 @@ public class CreateUserHandler : ICreateUserHandler
 {
 	private readonly IUnitofWork _unitofWork;
 	private readonly IMapper _mapper;
-	
-	public CreateUserHandler(IUnitofWork unitofWork, IMapper mapper)
+	private readonly IValidator<CreateUserRequest> _validator;
+
+	public CreateUserHandler(
+		IUnitofWork unitofWork, 
+		IMapper mapper, 
+		IValidator<CreateUserRequest> validator)
 	{
 		_unitofWork = unitofWork;
 		_mapper = mapper;
+		_validator = validator;
 	}
-	
+
 	public async Task<CreateUserResponse> HandleAsync(CreateUserRequest request)
 	{
-		
-		var validator = new CreateUserValidator();
-		var result = validator.Validate(request);
+		var result = _validator.Validate(request);
 		/*TODO:
 			-implement best practice expection handler to give a feedback to user
 			-implement loger
 		*/
 		if(!result.IsValid)
 		{
-			return null;
+			_validator.ValidateAndThrow(request);
 		}
+		
 		var user = _mapper.Map<User>(request);
 		await _unitofWork.Repository<User>().CreateAsync(user);
+		
 		await _unitofWork.SaveChangeAsync();
 		
 		return _mapper.Map<CreateUserResponse>(user);
