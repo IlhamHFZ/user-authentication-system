60a34bd49b989c4dcb195ce80e72964d9168614e
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-10-19 18:50:20 +0700

    refactor code

--- src/webapi/Webapi/Middleware/ApiResponseMiddleware.cs
+++ src/webapi/Webapi/Middleware/ApiResponseMiddleware.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Json;
 
 namespace Webapi.Middleware;
@@ -10,39 +11,68 @@ public class ApiResponseMiddleware
 	{
 		_next = next;
 	}
-	
+
 	public async Task InvokeAsync(HttpContext context)
 	{
 		Stream originalBodyResponse = context.Response.Body;
-		
-		using(MemoryStream newBodyResponse = new MemoryStream())
+
+		using (MemoryStream newBodyResponse = new MemoryStream())
 		{
 			context.Response.Body = newBodyResponse;
-			
+
 			await _next(context);
-			
-			newBodyResponse.Seek(0, SeekOrigin.Begin);
-			string stringNewBody = await new StreamReader(newBodyResponse).ReadToEndAsync();
-			
-			newBodyResponse.Seek(0, SeekOrigin.Begin);
-			JsonWriterOptions jsonWriterOptions = new JsonWriterOptions()
+
+			if(IsJsonResponse(context.Response))
 			{
-				Indented = true
-			};
-			using(Utf8JsonWriter jsonWriter = new Utf8JsonWriter(newBodyResponse))
-			{
-				jsonWriter.WriteStartObject();
-				jsonWriter.WriteNumber("status", context.Response.StatusCode);
-				jsonWriter.WriteString("date", DateTime.Now.ToString("dddd, dd MM yyyy HH:mm:ss tt"));
-				jsonWriter.WritePropertyName("data");
-				jsonWriter.WriteRawValue(stringNewBody);
-				await jsonWriter.FlushAsync();
-				jsonWriter.WriteEndObject();
+				await WriteJsonApiResponse(context, originalBodyResponse);
 			}
-			
-			newBodyResponse.Seek(0, SeekOrigin.Begin);
-			await newBodyResponse.CopyToAsync(originalBodyResponse);
-			context.Response.Body = originalBodyResponse;
+			else
+			{
+				newBodyResponse.Seek(0, SeekOrigin.Begin);
+				await newBodyResponse.CopyToAsync(originalBodyResponse);
+				context.Response.Body = originalBodyResponse;
+			}
+
 		}
+	}
+
+	private async Task WriteJsonApiResponse(HttpContext context, Stream originalBodyResponse)
+	{
+		context.Response.Body.Seek(0, SeekOrigin.Begin);
+		StringBuilder stringNewBody = new StringBuilder(await new StreamReader(context.Response.Body).ReadToEndAsync());
+		StringBuilder message = new StringBuilder();
+		
+		if(context.Response.StatusCode == 200)
+		{
+			message.Append("success");
+		}
+		
+		if(context.Response.StatusCode == 404)
+		{
+			message.Append(stringNewBody.ToString());
+			message.Replace("\"", "");
+			stringNewBody.Clear();
+		}
+			
+		var response = new
+		{
+			Status = context.Response.StatusCode,
+			Message = message.ToString(),
+			Date = DateTime.Now.ToString("dddd, dd-MM-yyyy HH:mm:ss tt"),
+			Data = stringNewBody.Length != 0 ? JsonSerializer.Deserialize<object>(stringNewBody.ToString()) : null
+		};
+		
+		context.Response.Body.Seek(0, SeekOrigin.Begin);
+		await context.Response.WriteAsJsonAsync(response);
+		
+		context.Response.Body.Seek(0, SeekOrigin.Begin);
+		await context.Response.Body.CopyToAsync(originalBodyResponse);
+		
+		context.Response.Body = originalBodyResponse;
+	}
+
+	private bool IsJsonResponse(HttpResponse httpResponse)
+	{
+		return httpResponse.ContentType != null && httpResponse.ContentType.Contains("application/json");
 	}
 }
