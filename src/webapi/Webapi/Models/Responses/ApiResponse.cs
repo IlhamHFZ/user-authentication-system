72f857ead00016541cacfdc1f683b0d86f94d0d4
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-08 17:04:03 +0700

    add Filtering, Sorting, and Pagination as format api response

--- src/webapi/Webapi/Models/Responses/ApiResponse.cs
+++ src/webapi/Webapi/Models/Responses/ApiResponse.cs
@@ -1,3 +1,6 @@
+using System.Text.Json.Serialization;
+using Application.Shared;
+
 namespace Webapi.Models.Responses;
 
 public record ApiResponse<T> where T: class
@@ -6,4 +9,13 @@ public record ApiResponse<T> where T: class
 	public string Message {get; set;} = null!;
 	public string Date {get; set;} = DateTime.Now.ToString("dddd, dd-MM-yyyy HH:mm:ss tt");
 	public T? Data {get; set;}
+	
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public Filtering? Filtering {get; set;}
+	
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public Sorting? Sorting {get; set;}
+	
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public Pagination? Pagination {get; set;}
 }
