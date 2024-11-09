0617e644e90bc7b85d43bf34f585d2b134f2cde9
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-09 22:22:54 +0700

    add IsSuccess and Errors property in UpdateUserProfileResponse

--- src/core/Application/Features/UserFeatures/UpdateUserProfile/UpdateUserProfileResponse.cs
+++ src/core/Application/Features/UserFeatures/UpdateUserProfile/UpdateUserProfileResponse.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace Application.Features.UserFeatures.UpdateUserProfile;
 
 public record class UpdateUserProfileResponse
@@ -5,4 +7,6 @@ public record class UpdateUserProfileResponse
 	public Guid Id { get; set; }
 	public string UserName {get; set;} = null!;
 	public string DisplayName {get; set;} = null!;
+	public bool IsSuccess {get; set;}
+	public IEnumerable<IdentityError>? Errors {get; set;}
 }
