07aacc22876ce0b9b19faf8092451ccb90c4fa93
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-09 22:17:44 +0700

    refacory GetByIdHandler.HandlerAsync to use UserManager

--- src/core/Application/Features/UserFeatures/GetByIdUser/GetByIdUserHandler.cs
+++ src/core/Application/Features/UserFeatures/GetByIdUser/GetByIdUserHandler.cs
@@ -2,41 +2,31 @@ using Application.Features.UserFeatures.Interface;
 using AutoMapper;
 using Domain.Entites;
 using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
-using Presistence.Repository.Interface;
 
 namespace Application.Features.UserFeatures.GetByIdUser;
 
 public class GetByIdUserHandler : IGetByIdUserHandler
 {
-	private readonly IUnitofWork _unitofWork;
+	private readonly UserManager<User> _userManager;
 	private readonly IMapper _mapper;
-	private readonly IValidator<GetByIdUserRequest> _validator;
 	private readonly ILogger<GetByIdUserHandler> _logger;
 
 	public GetByIdUserHandler(
-		IUnitofWork unitofWork,
+		UserManager<User> userManager,
 		IMapper mapper,
-		IValidator<GetByIdUserRequest> validator,
 		ILogger<GetByIdUserHandler> logger)
 	{
-		_unitofWork = unitofWork;
+		_userManager = userManager;
 		_mapper = mapper;
-		_validator = validator;
 		_logger = logger;
 	}
 
 	public async Task<GetByIdUserResponse?> HandleAsync(GetByIdUserRequest request)
 	{
 		_logger.LogInformation($"Starting to process GetByIdUserHandler request for user with id {request.Id}");
-		var result = _validator.Validate(request);
-		if(!result.IsValid)
-		{
-			_logger.LogWarning($"Validation failed for GetByIdUserHandler request for user with id {request.Id}");
-			_validator.ValidateAndThrow(request);
-		}
-		
-		var user = await _unitofWork.Repository<User>().GetAsync(request.Id);
+		var user = await _userManager.FindByIdAsync(request.Id.ToString());
 		if(user is null)
 		{
 			_logger.LogWarning($"User not found for user with id {request.Id}");
