fe54d298ae78aaa59d00bc09d088033816d971de
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-09 22:16:45 +0700

    add NormalizeUserName, NormalizeDisplayName, and NormalizeEmail property in GetByIdResponse.cs

--- src/core/Application/Features/UserFeatures/GetByIdUser/GetByIdUserResponse.cs
+++ src/core/Application/Features/UserFeatures/GetByIdUser/GetByIdUserResponse.cs
@@ -4,10 +4,12 @@ public record GetByIdUserResponse
 {
 	public Guid Id { get; set; }
 	public string UserName { get; set; } = null!;
+	public string NormalizedUserName {get; set;} = null!;
 	public string DisplayName { get; set; } = null!;
+	public string NormalizeDisplayName {get; set;} = null!;
 	public string Email { get; set; } = null!;
+	public string NormalizedEmail {get; set;} = null!;
 	public bool EmailConfirmed { get; set; }
 	public string? PhoneNumber {get; set; }
 	public bool PhoneConfirmed { get; set; }
-	public bool TwoFactorEnable { get; set; }
 }
