0ce6e3cf457e7ca4fdd2b6a5f84649abf725c643
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-11 20:14:45 +0700

    add mapping from CreateRoleRequest to Role, from IdentityResult to CreateUserResponse

--- src/core/Application/Features/RoleFeatures/CreateRole/CreateRoleMapper.cs
+++ src/core/Application/Features/RoleFeatures/CreateRole/CreateRoleMapper.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Domain.Entites;
+using Microsoft.AspNetCore.Identity;
 
 namespace Application.Features.RoleFeatures.CreateRole;
 
@@ -7,6 +8,11 @@ public class CreateRoleMapper : Profile
 {
 	public CreateRoleMapper()
 	{
-		CreateMap<Role, CreateRoleResponse>().ReverseMap();
+		CreateMap<CreateRoleRequest, Role>()
+			.ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.RoleName));
+		CreateMap<Role, CreateRoleResponse>()
+			.ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Name));
+		CreateMap<IdentityResult, CreateRoleResponse>()
+			.ForMember(dest => dest.IsSuccess, opt => opt.MapFrom(src => src.Succeeded));
 	}
 }
