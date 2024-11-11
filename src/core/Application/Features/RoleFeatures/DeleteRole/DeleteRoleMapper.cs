5e746d3a83f7f1d297639052f365182ed8870ad7
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-11 14:24:37 +0700

    add mapping from IdentityResult to DeleteRoleResponse

--- src/core/Application/Features/RoleFeatures/DeleteRole/DeleteRoleMapper.cs
+++ src/core/Application/Features/RoleFeatures/DeleteRole/DeleteRoleMapper.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Domain.Entites;
+using Microsoft.AspNetCore.Identity;
 
 namespace Application.Features.RoleFeatures.DeleteRole;
 
@@ -7,6 +8,9 @@ public class DeleteRoleMapper : Profile
 {
 	public DeleteRoleMapper()
 	{
-		CreateMap<Role, DeleteRoleResponse>();
+		CreateMap<Role, DeleteRoleResponse>()
+			.ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Name));
+		CreateMap<IdentityResult, DeleteRoleResponse>()
+			.ForMember(dest => dest.IsSuccess, opt => opt.MapFrom(src => src.Succeeded));
 	}
 }
