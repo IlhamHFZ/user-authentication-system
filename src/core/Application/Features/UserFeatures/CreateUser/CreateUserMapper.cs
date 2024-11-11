048a5d2ce734e0a8de51cea1f78dda5222a72ea3
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-11 20:08:15 +0700

    add mapping from CreateUserRequest to User

--- src/core/Application/Features/UserFeatures/CreateUser/CreateUserMapper.cs
+++ src/core/Application/Features/UserFeatures/CreateUser/CreateUserMapper.cs
@@ -8,8 +8,7 @@ public class CreateUserMapper : Profile
 {
 	public CreateUserMapper()
 	{
-		CreateMap<CreateUserRequest, User>()
-			.ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.Password));
+		CreateMap<CreateUserRequest, User>();
 		CreateMap<User, CreateUserResponse>();
 		CreateMap<IdentityResult, CreateUserResponse>()
 			.ForMember(dest => dest.IsSuccess, opt => opt.MapFrom(src => src.Succeeded));
