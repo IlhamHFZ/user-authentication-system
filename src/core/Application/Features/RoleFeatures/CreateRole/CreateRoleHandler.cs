d7d4314141027e2745624963b6f6df95f68031b0
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-11 20:15:56 +0700

    refactory CreateRoleHandler using rolemanager

--- src/core/Application/Features/RoleFeatures/CreateRole/CreateRoleHandler.cs
+++ src/core/Application/Features/RoleFeatures/CreateRole/CreateRoleHandler.cs
@@ -1,49 +1,44 @@
 using Application.Features.RoleFeatures.Interface;
 using AutoMapper;
 using Domain.Entites;
-using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
-using Presistence.Repository.Interface;
 
 namespace Application.Features.RoleFeatures.CreateRole;
 
 public class CreateRoleHandler : ICreateRoleHandler
 {
-	private readonly IUnitofWork _unitofWork;
+	private readonly RoleManager<Role> _roleManager;
 	private readonly IMapper _mapper;
-	private readonly IValidator<CreateRoleRequest> _validator;
 	private readonly ILogger<CreateRoleHandler> _logger;
 
 	public CreateRoleHandler(
-		IUnitofWork unitofWork, 
+		RoleManager<Role> roleManager,
 		IMapper mapper, 
-		IValidator<CreateRoleRequest> validator, 
 		ILogger<CreateRoleHandler> logger)
 	{
-		_unitofWork = unitofWork;
+		_roleManager = roleManager;
 		_mapper = mapper;
-		_validator = validator;
 		_logger = logger;
 	}
 
 	public async Task<CreateRoleResponse> HandleAsync(CreateRoleRequest request)
 	{
-		_logger.LogInformation($"Starting to process CreateRoleHandler request for role with name {request.Name}");
-		var result = await _validator.ValidateAsync(request);
-		if(!result.IsValid)
+		_logger.LogInformation($"Starting to process CreateRoleHandler request for role with name {request.RoleName}");
+		var newRole = _mapper.Map<Role>(request);
+		
+		_logger.LogInformation($"Create role for user with name {request.RoleName}");
+		var identityResult = await _roleManager.CreateAsync(newRole);
+		var response = _mapper.Map<CreateRoleResponse>(identityResult);
+		response = _mapper.Map(newRole, response);
+		if(!identityResult.Succeeded)
 		{
-			_logger.LogWarning($"Validation failed for CreateRoleHandler request for role with name {request.Name}");
-			await _validator.ValidateAndThrowAsync(request);
+			_logger.LogWarning($"Failed to create role for role with name {request.RoleName}");			
+			return response;
 		}
 		
-		var role = _mapper.Map<Role>(request);
 		
-		await _unitofWork.Repository<Role>().CreateAsync(role);
-		_logger.LogInformation($"Saving new role to database for role with name {request.Name}");
-		
-		await _unitofWork.SaveChangeAsync();
-		_logger.LogInformation($"Role successfullt saved in database for role with name {request.Name}");
-		
-		return _mapper.Map<CreateRoleResponse>(role);
+		_logger.LogInformation($"User successfully create in database for role with name {request.RoleName}");
+		return response;
 	}
 }
