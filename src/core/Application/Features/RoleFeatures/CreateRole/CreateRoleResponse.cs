564c8adb40be08576e07f6568ea4f1043be615e1
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-11 20:13:38 +0700

    add IsSuccess and Errors property in CreateRoleResponse

--- src/core/Application/Features/RoleFeatures/CreateRole/CreateRoleResponse.cs
+++ src/core/Application/Features/RoleFeatures/CreateRole/CreateRoleResponse.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace Application.Features.RoleFeatures.CreateRole;
 
 public record CreateRoleResponse
 {
-	public Guid Id {get; set;}
+	public Guid? Id {get; set;}
 	public string RoleName {get; set;} = null!;
+	public bool IsSuccess {get; set;}
+	public IEnumerable<IdentityError>? Errors {get; set;}
 }
