87d67babd77a978a63f3932684b6071906407742
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-11 14:23:31 +0700

    add Errors and IsSuccess property in DeleteRoleResponse

--- src/core/Application/Features/RoleFeatures/DeleteRole/DeleteRoleResponse.cs
+++ src/core/Application/Features/RoleFeatures/DeleteRole/DeleteRoleResponse.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace Application.Features.RoleFeatures.DeleteRole;
 
 public record DeleteRoleResponse
 {
 	public Guid Id {get; set;}
 	public string RoleName {get; set;} = null!;
+	public bool IsSuccess {get; set;}
+	public IEnumerable<IdentityError>? Errors {get; set;}
 }
