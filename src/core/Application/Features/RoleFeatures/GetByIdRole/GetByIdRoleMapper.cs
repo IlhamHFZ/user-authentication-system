c78917eb95ee19183b30d35c28d1020e68e36929
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-11 12:44:09 +0700

    edit mapping from role to GetByIdRoleResponse

--- src/core/Application/Features/RoleFeatures/GetByIdRole/GetByIdRoleMapper.cs
+++ src/core/Application/Features/RoleFeatures/GetByIdRole/GetByIdRoleMapper.cs
@@ -7,6 +7,8 @@ public class GetByIdRoleMapper : Profile
 {
 	public GetByIdRoleMapper()
 	{
-		CreateMap<Role, GetByIdRoleResponse>();
+		CreateMap<Role, GetByIdRoleResponse>()
+			.ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Name))
+			.ForMember(dest => dest.NormalizeRoleName, opt => opt.MapFrom(src => src.NormalizedName));
 	}
 }
