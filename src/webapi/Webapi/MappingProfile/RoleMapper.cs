1d5794b2a6b6546fef6dd1b37caee6aca852ab4c
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-11 14:02:01 +0700

    add mapping from getallroleresponse to api format getallrolesuccessresponse for getallrole endpoint

--- src/webapi/Webapi/MappingProfile/RoleMapper.cs
+++ src/webapi/Webapi/MappingProfile/RoleMapper.cs
@@ -1,3 +1,4 @@
+using Application.Features.RoleFeatures.GetAllRole;
 using Application.Features.RoleFeatures.GetByIdRole;
 using AutoMapper;
 using Webapi.Models.Responses.Role;
@@ -9,5 +10,7 @@ public class RoleMapper : Profile
 	public RoleMapper()
 	{
 		CreateMap<GetByIdRoleResponse, GetByIdRoleSuccessResponse>();
+		
+		CreateMap<GetAllRoleResponse, GetAllRoleSuccessResponse>();
 	}
 }
